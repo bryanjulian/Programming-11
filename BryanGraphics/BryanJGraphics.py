e4def5171e6d271de57f9bd41936ff197aa05749
bryanjulian <reshiramzekrom@hotmail.ca>
2017-06-19 12:48:25 -0700

    Add files via upload

---
+++ BryanGraphics/BryanJGraphics.py
@@ -0,0 +1,55 @@
+import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+player_image = pygame.image.load("player.bmp").convert()
+background_image = pygame.image.load("saturn_family1.bmp").convert() 
+click_sound = pygame.mixer.Sound("laser5.ogg")
+player_image.set_colorkey(WHITE)
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+    # Get the current mouse position. This returns the position
+    # as a list of two numbers.
+    player_position = pygame.mouse.get_pos()
+    x = player_position[0]
+    y = player_position[1]    
+    pygame.mouse.set_visible(False)
+    screen.blit(background_image, [0, 0]) 
+    screen.blit(player_image, [x, y])  
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click_sound.play()    
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
